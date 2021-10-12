2b14adb9351d2fcbdf5895fa31aaf7258575ca79
b3tts32 <rodneybetts@gmail.com>
2021-10-11 19:14:13 -0500

    Update 01-providers.tf

--- 01-providers.tf
+++ 01-providers.tf
@@ -1,35 +1,35 @@
-variable "pingdom_user" {}
-variable "pingdom_password" {}
-variable "pingdom_api_key" {}
-variable "cloudflare_zone_id" {}
+# variable "pingdom_user" {}
+# variable "pingdom_password" {}
+# variable "pingdom_api_key" {}
+# variable "cloudflare_zone_id" {}
 
-provider "azurerm" {
-  version = "=2.8.0"
+# provider "azurerm" {
+#   version = "=2.8.0"
 
-  features {}
-}
+#   features {}
+# }
 
-provider "k8s" {
-  load_config_file = "false"
-}
+# provider "k8s" {
+#   load_config_file = "false"
+# }
 
-provider "cloudflare" {
-  version = "=2.6.0"
-}
+# provider "cloudflare" {
+#   version = "=2.6.0"
+# }
 
-provider "vault" {
-}
+# provider "vault" {
+# }
 
-provider "pingdom" {
-    user = "${var.pingdom_user}"
-    password = "${var.pingdom_password}"
-    api_key = "${var.pingdom_api_key}"
-}
+# provider "pingdom" {
+#     user = "${var.pingdom_user}"
+#     password = "${var.pingdom_password}"
+#     api_key = "${var.pingdom_api_key}"
+# }
 
-terraform {
-  backend "azurerm" {
-    storage_account_name = "demo"
-    container_name       = "tfstate"
-    key                  = "demo.terraform.tfstate"
-  }
-}
+# terraform {
+#   backend "azurerm" {
+#     storage_account_name = "demo"
+#     container_name       = "tfstate"
+#     key                  = "demo.terraform.tfstate"
+#   }
+# }
